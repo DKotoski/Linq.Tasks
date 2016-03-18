ac3bee05df342c362df49fc9b2bf5713bbdfeafc
Darko Kotoski <wbountyhunter@hotmail.com>
2016-03-18 18:07:39 +0100

    initial commit
    asdasd

---
+++ Linq.Tasks/Program.cs
@@ -0,0 +1,89 @@
+﻿using Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Linq.Tasks
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Product> products = InitProducts();
+
+            var namesOfProducts = products.Select(p => p.Name);
+            Console.ReadLine();
+        }
+        static List<Product> InitProducts()
+        {
+            var products = new List<Product>();
+            products.Add(new Product {
+                Name = "Banana",
+                Category = "Fruit",
+                ID = 0,
+                UnitPrice = 3,
+                UnitsInStock = 10
+            });
+            products.Add(new Product
+            {
+                Name = "Apple",
+                Category = "Fruit",
+                ID = 1,
+                UnitPrice = 2,
+                UnitsInStock = 5
+            });
+            products.Add(new Product
+            {
+                Name = "Pepper",
+                Category = "Vegetable",
+                ID = 2,
+                UnitPrice = 4,
+                UnitsInStock = 13
+            });
+            products.Add(new Product
+            {
+                Name = "Potato",
+                Category = "Vegetable",
+                ID = 3,
+                UnitPrice = 6,
+                UnitsInStock = 4
+            });
+            products.Add(new Product
+            {
+                Name = "Tooth Paste",
+                Category = "Higiene",
+                ID = 4,
+                UnitPrice = 6,
+                UnitsInStock = 0
+            });
+            products.Add(new Product
+            {
+                Name = "Car Tire",
+                Category = "Autos",
+                ID = 5,
+                UnitPrice = 12,
+                UnitsInStock = 3
+            });
+            products.Add(new Product
+            {
+                Name = "Juice",
+                Category = "Drinks",
+                ID = 6,
+                UnitPrice = 2,
+                UnitsInStock = 5
+            });
+            products.Add(new Product
+            {
+                Name = "Soap",
+                Category = "Higiene",
+                ID = 7,
+                UnitPrice = 3,
+                UnitsInStock = 10
+            });
+
+            return products;
+        }
+    }
+}
---
+++ Model/Product.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Model
+{
+    public class Product
+    {
+        public int UnitsInStock { get; set; }
+        public int UnitPrice { get; set; }
+        public string Name { get; set; }
+        public int ID { get; set; }
+        public string Category { get; set; }
+    }
+}
